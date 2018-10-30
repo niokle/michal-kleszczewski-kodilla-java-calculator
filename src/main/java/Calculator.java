063d71a69963f5f3c20d1b073190ebb812701010
Michał Kleszczewski <michalkleszczewski@macbook-pro-micha.home>
2018-10-30 10:18:04 +0100

    gotowe

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public double add(double value1, double value2) {
+        return value1 + value2;
+    }
+
+    public double substract(double value1, double value2) {
+        return value1 - value2;
+    }
+}
